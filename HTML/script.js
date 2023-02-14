555c75dcd2c49fb054bc9dceccc15efbdd32f807
Devarsh Joshi <skey.devarshjoshi@gmail.com>
2023-02-14 16:25:29 +0530

    commit

--- HTML/script.js
+++ HTML/script.js
@@ -1,3 +1,42 @@
+// portfolio lightbox
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
 // Accordion
 var acc = document.getElementsByClassName("accordions");
 var i;
@@ -16,7 +55,7 @@ for (i = 0; i < acc.length; i++) {
 
 // dropdown
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("dropdown-show");
 }
 
 window.onclick = function (event) {
@@ -47,44 +86,6 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-// portfolio lightbox
-
-// function openModal() {
-//   document.getElementById("myModal").style.display = "block";
-// }
-
-// function closeModal() {
-//   document.getElementById("myModal").style.display = "none";
-// }
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   var captionText = document.getElementById("caption");
-//   if (n > slides.length) { slideIndex = 1 }
-//   if (n < 1) { slideIndex = slides.length }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-//   dots[slideIndex - 1].className += " active";
-//   captionText.innerHTML = dots[slideIndex - 1].alt;
-// }
 
 // validation
 function validateForm() {
@@ -142,25 +143,10 @@ function showSlides(n) {
 window.onload = function () {
   setInterval(function () {
     plusSlides(1);
-  }, 2500);
+  }, 2000);
 };
 
-// scroll to top
-let mybutton = document.getElementById("myBtn");
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 // search bar
 function search_shoes() {
   let input = document.getElementById('searchbar').value
@@ -177,3 +163,137 @@ function search_shoes() {
   }
 }
 
+// team members tabb
+function openTeam(evt, positionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(positionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// carousel
+
+let slideIndice = 0;
+showSlide();
+
+function showSlide() {
+  let i;
+  let slide = document.getElementsByClassName("car");
+  let dot = document.getElementsByClassName("dots");
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slideIndice++;
+  if (slideIndice > slide.length) { slideIndice = 1 }
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
+  }
+  slide[slideIndice - 1].style.display = "block";
+  dot[slideIndice - 1].className += " active";
+  setTimeout(showSlide, 2000); // Change image every 2 seconds
+}
+
+// Header and footer animation 
+
+var greet = new Array("Sneakeround", "Sneak Around");
+
+var counter = 0;
+document.getElementById('greeting').innerHTML = greet[counter];
+
+Changegreeting1();
+function Changegreeting1() {
+  incrementIndex()
+  document.getElementById('greeting1').innerHTML = greet[counter];
+
+  document.getElementById('greeting').style.opacity = 0;
+
+  document.getElementById('greeting1').style.opacity = 1;
+
+  setTimeout(Changegreeting, 2000);
+}
+function Changegreeting() {
+  incrementIndex();
+  document.getElementById('greeting').innerHTML = greet[counter];
+
+  document.getElementById('greeting').style.opacity = 1;
+
+  document.getElementById('greeting1').style.opacity = 0;
+
+  setTimeout(Changegreeting1, 2000);
+}
+function incrementIndex() {
+  if (counter < greet.length - 1) {
+    counter++;
+  } else {
+    counter = 0;
+  }
+}
+
+// modal
+var modal = document.getElementById("myModals");
+
+var btn = document.getElementById("myButn");
+
+var span = document.getElementsByClassName("off")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// sticky header
+window.onscroll = function () { stickyFunction(scrollFunction()) };
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function stickyFunction(callback) {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+  callback()
+}
+
+//up top button
+let mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// hamburger menu
+function hamFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
